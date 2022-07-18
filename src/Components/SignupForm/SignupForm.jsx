7cb0e87f09e53f844713e0134623767ac7bcbdd8
afisman <afisman@gmail.com>
2022-07-18 19:36:24 +0200

    started register form, created context and services for aut

--- src/Components/SignupForm/SignupForm.jsx
+++ src/Components/SignupForm/SignupForm.jsx
@@ -1,15 +1,69 @@
-const SignupForm = () => {
-    // const [singupData, setSignupData] = useState({
-    //     username: '',
-    //     email: '',
-    //     password: ''
+import { useContext, useState } from "react"
+import { Form, Button } from "react-bootstrap"
+import { useNavigate } from 'react-router-dom'
+import { MessageContext } from "../../Context/userMessage.context"
+import authService from "./../../Services/auth.services"
 
-    // })
+const SignupForm = () => {
+    const [signupData, setSignupData] = useState({
+        username: '',
+        email: '',
+        password: ''
+
+    })
+
+    const { setShowMessage } = useContext(MessageContext)
+
+    const navigate = useNavigate()
+
+    const handleInputChange = e => {
+        const { value, name } = e.target
+        setSignupData({ ...signupData, [name]: value })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        authService
+            .signup(signupData)
+            .then(({ data }) => {
+                setShowMessage({ show: true, title: `Welcome ${data.user.username}`, text: 'You have registered correctly' })
+                navigate('/')
+            })
+            .catch(err => console.log(err))
+    }
+
+    const { username, password, email } = signupData
+
 
 
 
     return (
-        <h1>Sign up form</h1>
+        <Form onSubmit={handleSubmit}>
+
+            <Form.Group className="mb-3" controlId="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" value={username} onChange={handleInputChange} name="username" />
+            </Form.Group>
+
+
+            <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" value={password} onChange={handleInputChange} name="password" />
+            </Form.Group>
+
+
+            <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" value={email} onChange={handleInputChange} name="email" />
+            </Form.Group>
+
+
+            <div className="d-grid">
+                <Button variant="dark" type="submit">Register</Button>
+            </div>
+
+        </Form>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { MessageProviderWrapper } from './Context/userMessage.context';
+import { AuthProviderWrapper } from './Context/auth.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProviderWrapper>
+        <MessageProviderWrapper>
+          <App />
+        </MessageProviderWrapper>
+      </AuthProviderWrapper>
     </Router>
   </React.StrictMode>
 );
