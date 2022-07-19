5cecb7d6d5e3f0e756b67e8686166009268982b2
afisman <afisman@gmail.com>
2022-07-19 13:41:08 +0200

    added interceptor for services

--- src/Services/project.services.js
+++ src/Services/project.services.js
@@ -7,6 +7,17 @@ class ProjectService {
         this.api = axios.create({
             baseURL: `${process.env.REACT_APP_API_URL}/projects`
         })
+
+        this.api.interceptors.request.use((config) => {
+
+            const storedToken = localStorage.getItem("authToken");
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
     }
 
     getProjects() {
