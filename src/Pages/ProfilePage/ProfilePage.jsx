060dccfa71531987932aa74de6b199d4616102fc
Ghers Fisman <ghersf91@gmail.com>
2022-07-21 22:26:58 +0200

    profile done, except route protection and interests

--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,18 +1,63 @@
 import { Link, useParams } from "react-router-dom"
-import UserEditPage from "./../UserEditPage/UserEditPage"
+import userService from "../../Services/user.services"
+import { useEffect, useState } from "react"
+import { Button, Card } from 'react-bootstrap'
+
 
 const ProfilePage = () => {
 
     const { user_id } = useParams()
 
+    const [userData, setUserData] = useState({
+        username: '',
+        email: '',
+        bio: '',
+        role: '',
+        projectTypeInterests: '',
+        locationInterests: '',
+        profilePicture: '',
+    })
+
+    useEffect(() => {
+        loadUser()
+    }, [])
+
+    const loadUser = () => {
+        userService
+            .getUser(user_id)
+            .then(({ data }) => {
+                setUserData(data)
+            })
+            .catch(err => console.log(err))
+    }
+
+    const { username, email, bio, role, projectTypeInterests, locationInterests, profilePicture } = userData
+
     return (
         <>
-            <h1>Soy la página de perfil!</h1>
-            <Link to={`/users/editUser/${user_id}`}>
-                <div className="d-grid">
-                    <p>Edit user</p>
-                </div>
-            </Link>
+            <h1>Welcome, {username}!</h1>
+
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={profilePicture} />
+                <Card.Body>
+                    <Card.Title>{role}</Card.Title>
+                    <Card.Text>
+                        {bio}
+                    </Card.Text>
+                    <Card.Text>
+                        {email}
+                    </Card.Text>
+                    <Card.Text>
+                        Interested in: {projectTypeInterests}
+                    </Card.Text>
+                    <Card.Text>
+                        I want to go to: {locationInterests}
+                    </Card.Text>
+                    <Link to={`/users/editUser/${user_id}`}>
+                        <Button variant="primary">Update user information</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
         </>
 
     )
