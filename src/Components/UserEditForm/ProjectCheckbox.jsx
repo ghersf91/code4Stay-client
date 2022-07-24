8381bf5e0b1ce5661b4c4fad67b5022bc2f73552
afisman <afisman@gmail.com>
2022-07-24 21:42:09 +0200

    created project checkbox, still decoupled

---
+++ src/Components/UserEditForm/ProjectCheckbox.jsx
@@ -0,0 +1,160 @@
+const ProjectCheckbox = ({ receiveProjects, projectsChecked }) => {
+
+    const handleInputChange = e => {
+
+        const { name, checked } = e.target
+
+        const currentProjectInterests = [...projectsChecked]
+
+        if (checked && !currentProjectInterests.includes(name)) {
+
+            currentProjectInterests.push(name)
+
+        } else if (currentProjectInterests.includes(name)) {
+
+            const projectInterestsIndex = currentProjectInterests.indexOf(name)
+
+            projectInterestsIndex > -1 && currentProjectInterests.splice(projectInterestsIndex, 1)
+            console.log(currentProjectInterests)
+        }
+        receiveProjects(currentProjectInterests)
+    }
+    return (
+        <>
+            <Form.Group className='mb-3' controlId='projectTypeInterests' name='projectTypeInterests' onChange={handleInputChange}>
+                <Form.Label>Project interests</Form.Label>
+                <div key={`inline-checkbox`} className="mb-3">
+                    {
+                        projectsChecked.length && projectsChecked.includes('Farm')
+                            ?
+                            <Form.Check
+                                inline
+                                defaultChecked
+                                label="Farm"
+                                name="Farm"
+                                type={`checkbox`}
+                                id={"Farm"}
+                            />
+                            :
+                            <Form.Check
+                                inline
+                                label="Farm"
+                                name="Farm"
+                                type={`checkbox`}
+                                id={"Farm"}
+                            />
+                    }
+
+                    {
+                        projectsChecked.length && projectsChecked.includes('NGO')
+                            ?
+                            <Form.Check
+                                inline
+                                defaultChecked
+                                label="NGO"
+                                name="NGO"
+                                type={`checkbox`}
+                                id={"NGO"}
+                            />
+                            :
+                            <Form.Check
+                                inline
+                                label="NGO"
+                                name="NGO"
+                                type={`checkbox`}
+                                id={"NGO"}
+                            />
+                    }
+
+                    {
+                        projectsChecked.length && projectsChecked.includes('School')
+                            ?
+                            <Form.Check
+                                inline
+                                defaultChecked
+                                label="School"
+                                name="School"
+                                type={`checkbox`}
+                                id={"School"}
+                            />
+                            :
+                            <Form.Check
+                                inline
+                                label="School"
+                                name="School"
+                                type={`checkbox`}
+                                id={"School"}
+                            />
+                    }
+
+                    {
+                        projectsChecked.length && projectsChecked.includes('Hostel')
+                            ?
+                            <Form.Check
+                                inline
+                                defaultChecked
+                                label="Hostel"
+                                name="Hostel"
+                                type={`checkbox`}
+                                id={"Hostel"}
+                            />
+                            :
+                            <Form.Check
+                                inline
+                                label="Hostel"
+                                name="Hostel"
+                                type={`checkbox`}
+                                id={"Hostel"}
+                            />
+                    }
+
+                    {
+                        projectsChecked.length && projectsChecked.includes('Camping')
+                            ?
+                            <Form.Check
+                                inline
+                                defaultChecked
+                                label="Camping"
+                                name="Camping"
+                                type={`checkbox`}
+                                id={"Camping"}
+                            />
+                            :
+                            <Form.Check
+                                inline
+                                label="Camping"
+                                name="Camping"
+                                type={`checkbox`}
+                                id={"Camping"}
+                            />
+                    }
+
+                    {
+                        projectsChecked.length && projectsChecked.includes('Camping')
+                            ?
+                            <Form.Check
+                                inline
+                                defaultChecked
+                                label="Other"
+                                name="Other"
+                                type={`checkbox`}
+                                id={"Other"}
+                            />
+                            :
+                            <Form.Check
+                                inline
+                                label="Other"
+                                name="Other"
+                                type={`checkbox`}
+                                id={"Other"}
+                            />
+                    }
+                </div>
+            </Form.Group>
+        </>
+
+    )
+}
+
+
+export default ProjectCheckbox
