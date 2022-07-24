130a4e0f7cdd39a755c2a4e8af7a17d1641f76f2
Ghers Fisman <ghersf91@gmail.com>
2022-07-24 12:26:05 +0200

    MapPage features the addresses of projects

---
+++ src/Pages/MapPage/MapPage.jsx
@@ -0,0 +1,34 @@
+import Marker from '../../Components/Marker/Marker'
+import projectsService from "../../Services/project.services"
+import CreateProjectForm from "../../Components/CreateProjectForm/CreateProjectForm"
+
+import { useState, useEffect } from "react"
+
+
+
+
+const MapPage = () => {
+
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        loadProjects()
+    }, [])
+
+    const loadProjects = () => {
+        projectsService
+            .getProjects()
+            .then(({ data }) => {
+                setProjects(data)
+                // setShowMessage({ show: true, title: 'Welcome', text: `The projects have loaded successfully` })
+            })
+            .catch(err => console.log(err))
+    }
+
+
+    return (
+        <Marker projects={projects} />
+    )
+}
+
+export default MapPage
