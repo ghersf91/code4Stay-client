1a6c9826e7ebef89618706921d1057ab327a8fdc
Ghers Fisman <ghersf91@gmail.com>
2022-07-25 13:00:00 +0200

    map shows all projects

--- src/Components/MarkerGoogle/MarkerGoogle.jsx
+++ src/Components/MarkerGoogle/MarkerGoogle.jsx
@@ -1,57 +1,32 @@
-// import { useState } from 'react'
-// import { Row, Col, Button } from 'react-bootstrap'
+import { Marker } from '@react-google-maps/api'
+import { useState } from 'react'
 
+const MarkerGoogle = ({ projects }) => {
+    const [selected, setSelected] = useState({})
 
-// const Marker = ({ projects }) => {
+    const onSelect = item => {
+        setSelected(item)
+    }
 
-//     let [latitude, setLatitude] = useState(-33.7560119)
-//     let [longitude, setLongitude] = useState(150.6038367)
-//     let [address, setAddress] = useState('')
+    return (
+        <>
+            {
+                projects.map(project => {
+                    return (
+                        <>
+                            <Marker key={project._id}
+                                position={{
+                                    lat: project.location.coordinates[0],
+                                    lng: project.location.coordinates[1]
+                                }}
+                            // onClick={() => console.log(project._id)}
+                            />
+                        </>
+                    )
+                })
+            }
+        </>
+    )
+}
 
-//     const updateCoordinates = (e) => {
-//         e.preventDefault()
-
-//         const encodedAddress = encodeURI(address)
-
-//         fetch('xxxxxxxxxxxxxxxxxxx')
-//             .then(response => response.json())
-//             .then(response => {
-//                 setLatitude(response.lat)
-//                 setLongitude(response.long)
-//             })
-//             .catch(err => console.log(err))
-//     }
-
-//     return (
-//         <div>
-//             The latitude is {latitude}
-//             The longitude is {longitude}
-
-//             <Button as="input" type="submit" value="Curitiba, Brazil" onSubmit={(e) => updateCoordinates(e)} />{' '}
-//         </div>
-
-
-//     )
-// }
-
-
-// export default Marker
-
-
-//     // <>
-//     //         <h1>The addresses of the projects are:</h1>
-//     //         <Row>
-//     //             {
-//     //                 projects.map(project => {
-//     //                     return (
-//     //                         <Col md={3} key={project._id} >
-//     //                             <ul>
-//     //                                 <li>{project.city}, {project.country}</li>
-//     //                             </ul>
-//     //                         </Col>
-//     //                     )
-//     //                 })
-
-//     //             }
-//     //         </Row>
-//     //     </>
+export default MarkerGoogle
